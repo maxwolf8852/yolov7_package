f3212b673b4ff4c6668f7e388a9a246b751e8916
Maxim <62403261+maxwolf8852@users.noreply.github.com>
2022-10-18 12:25:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.6',
 )
